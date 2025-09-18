c1e66aabe26b8a70f6eae195fb99658e8e5b439c
Juan Ramirez <juanfeliperamirezsanchez@gmail.com>
2025-09-18 09:05:46 -0500

    dev push

--- src/lib/env.js
+++ src/lib/env.js
@@ -2,7 +2,7 @@
 // Nota: Las que van al navegador deben ser NEXT_PUBLIC_*
 
 export const ENV = {
-  ADSENSE_CLIENT: process.env.NEXT_PUBLIC_ADSENSE_CLIENT || "ca-pub-0000000000000000",
+  ADSENSE_CLIENT: process.env.NEXT_PUBLIC_ADSENSE_CLIENT || "ca-pub-1997080032223631",
   ADSENSE_SLOT: process.env.NEXT_PUBLIC_ADSENSE_SLOT || "1234567890",
   ADSENSE_TEST: String(process.env.NEXT_PUBLIC_ADSENSE_TEST || "true") === "true",
 };
